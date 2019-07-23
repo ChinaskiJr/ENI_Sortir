daf68528beacdb67b4c9c5ba98d61ff7c9e37c34
rcolombier <robin.colombier@protonmail.com>
2019-07-23 21:58:30 +0200

    Set up development environment : frameworks, locale, db connection...

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {LOCALE_ID, NgModule} from '@angular/core';
+import { registerLocaleData } from '@angular/common';
+import localeFr from '@angular/common/locales/fr';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+// Set up the locale to french
+registerLocaleData(localeFr, 'fr');
 
 @NgModule({
   declarations: [
@@ -12,7 +17,9 @@ import { AppComponent } from './app.component';
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    {provide: LOCALE_ID, useValue: 'fr'},
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
