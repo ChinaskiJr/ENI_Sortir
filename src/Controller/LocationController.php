dfa7ce67833f5ca193f55c920147400678ee0f69
rcolombier <robin.colombier@protonmail.com>
2019-08-02 12:12:37 +0200

    New methods for Location and Pursuits

--- src/Controller/LocationController.php
+++ src/Controller/LocationController.php
@@ -2,9 +2,42 @@
 
 namespace App\Controller;
 
+use App\Entity\Location;
 use FOS\RestBundle\Controller\AbstractFOSRestController;
+use FOS\RestBundle\Controller\Annotations as Rest;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 
 class LocationController extends AbstractFOSRestController
 {
+    /**
+     * Get all locations of a City
+     *
+     * @param $city
+     * @return Location[]
+     *
+     * @Rest\Route("/locations/city/{city}")
+     * @Rest\View()
+     */
+    public function getLocationsByCitiesAction($city) {
+        $repository = $this->getDoctrine()->getRepository(Location::class);
+        $locations = $repository->findBy(array('city' => $city));
 
+        return $locations;
+    }
+
+    /**
+     * Post a new location
+     *
+     * @param Location $location
+     * @return Location
+     *
+     * @Rest\View()
+     * @ParamConverter("location", converter="fos_rest.request_body")
+     */
+    public function postLocationAction(Location $location) {
+        $em = $this->getDoctrine()->getManager();
+        $em->persist($location);
+        $em->flush();
+        return $location;
+    }
 }
--- src/Controller/PursuitController.php
+++ src/Controller/PursuitController.php
@@ -5,10 +5,13 @@ namespace App\Controller;
 use App\Entity\Pursuit;
 use FOS\RestBundle\Controller\AbstractFOSRestController;
 use FOS\RestBundle\Controller\Annotations as Rest;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 
 class PursuitController extends AbstractFOSRestController
 {
     /**
+     * Get all pursuits proposed by a Site
+     *
      * @param $site
      * @return Pursuit[]
      *
@@ -20,5 +23,21 @@ class PursuitController extends AbstractFOSRestController
         $pursuits = $repository->findBy(array('site' => $site));
 
         return $pursuits;
+    }
+
+    /**
+     * Post new Pursuit
+     *
+     * @param Pursuit $pursuit
+     * @return Pursuit
+     *
+     * @Rest\View()
+     * @ParamConverter("pursuit", converter="fos_rest.request_body")
+     */
+    public function postPursuitAction(Pursuit $pursuit) {
+        $em = $this->getDoctrine()->getManager();
+        $em->persist($pursuit);
+        $em->flush();
+        return $pursuit;
     }
 }
