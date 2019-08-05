377e699f7bd59f25d8310de1c9f0c22d5e1f41db
rcolombier <robin.colombier@protonmail.com>
2019-08-05 14:20:18 +0200

    Users can now manage the pursuits where they are the organizer

--- src/app/pursuit/pursuit.component.ts
+++ src/app/pursuit/pursuit.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import {Pursuit} from '../models/Pursuit';
 import {ActivatedRoute} from '@angular/router';
 import {PursuitsManagementService} from '../services/pursuits-management.service';
+import {Participant} from '../models/Participant';
+import {LoginManagementService} from '../services/login-management.service';
 
 @Component({
   selector: 'app-pursuit',
@@ -10,15 +12,20 @@ import {PursuitsManagementService} from '../services/pursuits-management.service
 })
 export class PursuitComponent implements OnInit {
   pursuit: Pursuit;
+  currentUser: Participant;
 
   constructor(private route: ActivatedRoute,
-              private pursuitManagement: PursuitsManagementService) { }
+              private pursuitManagement: PursuitsManagementService,
+              private loginManagement: LoginManagementService) { }
 
   ngOnInit() {
     this.pursuitManagement.getPursuitByNb(this.route.snapshot.params.nbPursuit).subscribe(
       value => {
         this.pursuit = value;
       }
+    );
+    this.loginManagement.currentUser.subscribe(
+      value => this.currentUser = value
     );
   }
 }
