453dcb01562bc20f72cb2d30b82e399a4c80d460
rcolombier <robin.colombier@protonmail.com>
2019-08-05 15:29:51 +0200

    Users can now desist themselves from pursuits

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -14,6 +14,8 @@ use Symfony\Component\HttpKernel\Exception\HttpException;
 class RegistrationController extends AbstractFOSRestController
 {
     /**
+     * Register a participant to a pursuit
+     *
      * @Rest\Route("/registration/pursuit/{nbPursuit}/participant/{nbParticipant}")
      * @Rest\View()
      * @param $nbPursuit
@@ -26,24 +28,30 @@ class RegistrationController extends AbstractFOSRestController
         $repository = $this->getDoctrine()->getRepository(Registration::class);
         $registration = new Registration();
         $registration->setDateRegistration(new DateTime());
+        // Get pursuit
         $pursuit = $this
             ->getDoctrine()
             ->getRepository(Pursuit::class)
             ->find($nbPursuit);
+        // Check pursuit
         if (empty($pursuit)) {
             throw new HttpException(404, 'Aucune sortie correspondante');
         }
         if ($pursuit->getState()->getWord() !== 'Ouvert') {
             throw new HttpException(403, 'Cette sortie n\'accepte pas d\'inscriptions');
         }
+        // Set pursuit
         $registration->setPursuit($pursuit);
+        // Get Participant
         $participant = $this
             ->getDoctrine()
             ->getRepository(Participant::class)
             ->find($nbParticipant);
+        // Get participant
         if (empty($participant)) {
             throw new HttpException(404, 'Aucun participant correspondant');
         }
+        // set participant
         $registration->setParticipant($participant);
         // Is our user already registered ?
         $qb = $repository->createQueryBuilder('r')
@@ -52,13 +60,56 @@ class RegistrationController extends AbstractFOSRestController
             ->andWhere('r.pursuit = :pursuit')
             ->setParameter('pursuit', $pursuit)
             ->getQuery();
-
         if (!empty($qb->execute())) {
             throw new HttpException(403, 'Participant déjà inscrit pour cette sortie');
         }
-
+        // All is cool, persist !
         $em->persist($registration);
         $em->flush();
         return $registration;
+    }
+    /**
+     * Remove a participant from a pursuit
+     *
+     * @Rest\Route("/registration/pursuit/{nbPursuit}/participant/{nbParticipant}")
+     * @Rest\View()
+     * @param $nbPursuit
+     * @param $nbParticipant
+     * @throws Exception
+     */
+    public function deleteRegistrationAction($nbPursuit, $nbParticipant) {
+        $em = $this->getDoctrine()->getManager();
+        $repository = $this->getDoctrine()->getRepository(Registration::class);
+        // Get pursuit
+        $pursuit = $this
+            ->getDoctrine()
+            ->getRepository(Pursuit::class)
+            ->find($nbPursuit);
+        // Check pursuit
+        if (empty($pursuit)) {
+            throw new HttpException(404, 'Aucune sortie correspondante');
+        }
+        if ($pursuit->getState()->getWord() !== 'Ouvert') {
+            throw new HttpException(403, 'Cette sortie n\'accepte pas de désistement');
+        }
+        // Get Participant
+        $participant = $this
+            ->getDoctrine()
+            ->getRepository(Participant::class)
+            ->find($nbParticipant);
+        // Get participant
+        if (empty($participant)) {
+            throw new HttpException(404, 'Aucun participant correspondant');
+        }
+        // Get our registration
+        $qb = $repository->createQueryBuilder('r')
+            ->where('r.participant = :participant')
+            ->setParameter('participant', $participant)
+            ->andWhere('r.pursuit = :pursuit')
+            ->setParameter('pursuit', $pursuit)
+            ->getQuery();
+        $registration = $qb->getSingleResult();
+        $em->remove($registration);
+        $em->flush();
     }
 }
