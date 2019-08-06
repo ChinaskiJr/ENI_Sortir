a3aa432a7fffbc236eb1dc8ccc5c40eaa6b086a8
rcolombier <robin.colombier@protonmail.com>
2019-08-06 12:06:27 +0200

    Organizer can now cancel their pursuits

--- src/app/pursuit/pursuit.component.ts
+++ src/app/pursuit/pursuit.component.ts
@@ -6,6 +6,7 @@ import {Participant} from '../models/Participant';
 import {LoginManagementService} from '../services/login-management.service';
 import {State} from '../models/State';
 import {StatesManagementService} from '../services/states-management.service';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-pursuit',
@@ -16,12 +17,15 @@ export class PursuitComponent implements OnInit {
   pursuit: Pursuit;
   currentUser: Participant;
   states: State[];
+  cancelForm: FormGroup;
 
   constructor(private route: ActivatedRoute,
               private pursuitManagement: PursuitsManagementService,
               private loginManagement: LoginManagementService,
               private stateManagement: StatesManagementService,
-              private router: Router) { }
+              private router: Router,
+              private formBuilder: FormBuilder) {
+  }
 
   ngOnInit() {
     this.pursuitManagement.getPursuitByNb(this.route.snapshot.params.nbPursuit).subscribe(
@@ -35,7 +39,28 @@ export class PursuitComponent implements OnInit {
     this.stateManagement.getAllStates().subscribe(
       value => this.states = value
     );
+    this.initForm();
+  }
 
+  initForm() {
+    this.cancelForm = this.formBuilder.group({
+      message: ['', [Validators.required, Validators.maxLength(30)]]
+    });
+  }
+
+  private invalidField(field: string) {
+    return this.cancelForm.controls[field].invalid && (
+      this.cancelForm.controls[field].dirty ||
+      this.cancelForm.controls[field].touched
+    );
+  }
+
+  private issueValidation() {
+    return this.cancelForm.invalid || this.cancelForm.pristine;
+  }
+
+  messageNotValid() {
+    return this.invalidField('message');
   }
 
   /**
@@ -46,7 +71,21 @@ export class PursuitComponent implements OnInit {
     this.pursuit.state = this.states.find((state) => state.word === 'Ouvert');
     // Update
     this.pursuitManagement.putPursuit(this.pursuit).subscribe(
-      value => console.log(value),
+      () => {},
+      () => {},
+      // When it's done, redirect to home
+      () => this.router.navigate(['/home'])
+    );
+  }
+
+  onCancel() {
+    // Get he id of the "Annulé" state
+    this.pursuit.state = this.states.find((state) => state.word === 'Annulé');
+    // Set the message in the description
+    this.pursuit.description = this.pursuit.description + '\n' + this.cancelForm.value.message;
+    // Update
+    this.pursuitManagement.putPursuit(this.pursuit).subscribe(
+      () => {},
       () => {},
       // When it's done, redirect to home
       () => this.router.navigate(['/home'])
